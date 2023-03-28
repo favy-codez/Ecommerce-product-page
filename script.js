59053cdd15967dc1fab787ce960633a7bf795d9c
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-03-28 07:41:16 +0100

    worked on js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
-const sign1 = document.getElementById("sign-1")
-const sign2 = document.getElementById("sign-2")
-const sign3 = document.getElementById("sign-3")
+const sign1 = document.getElementById("sign-1");
+const sign2 = document.getElementById("sign-2");
+const sign3 = document.getElementById("sign-3");
+const cartBtn = document.getElementById("cartButton");
